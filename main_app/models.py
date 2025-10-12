af9eb2841f4b46024fb03a13ee2cd0e71a0dc5de
Mousa Aricat <21110078@htu.edu.jo>
2025-10-12 11:48:53 +0300

    Feat: Creating the quests model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Quests(models.Model):
+    quest_id = models.BigAutoField(primary_key = True)
+    title = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 150)
+    category = models.CharField(max_length = 40, choices = [('study', 'Study'), ('fitness','Fitness'),('work','Work'),('habit','Habit'),('daily routine','Daily Routine'),('self care','Self Care'),('other','Other')])
+    difficulty = models.CharField(max_length = 30, choices = [('easy', 'Easy'), ('medium', 'Medium'), ('hard', 'Hard')])
+    reward_xp = models.IntegerField()
+    is_completed = models.BooleanField(default = False)
+    date_created = models.DateTimeField()
+    deadline = models.DateTimeField()
