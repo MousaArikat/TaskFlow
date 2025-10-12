412c6d7722458ae4bb68ec4950480b20e95a82d4
Mousa Aricat <21110078@htu.edu.jo>
2025-10-12 13:32:13 +0300

    Feat: Adding the create task function in the views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TaskForm
+from django.urls import reverse, reverse_lazy
+
+
 
 # Create your views here.
 def home_page(request):
     return render(request, 'main_app/homepage.html')
 
+
+def create_task(request):
+    if request.method == "POST":
+        form = TaskForm()
+        if form.is_valid():
+            task = form.save()
+            return redirect(reverse('task_list'))
+        else:
+            return render(request, 'main_app/task-list.html', {'form' : form})
+    elif request.method == "POST":
+        form = TaskForm()
+        return render(request, 'main_app/task-list.html', {'form' : form})
