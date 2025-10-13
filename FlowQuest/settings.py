3844e43698cef29d22700fbbeac71e59f2328d4c
Mousa Aricat <21110078@htu.edu.jo>
2025-10-13 17:52:10 +0300

    Feat: adding logout and changing the home page dynamically based on the user auth

--- FlowQuest/settings.py
+++ FlowQuest/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'homepage'
+LOGOUT_REDIRECT_URL = 'homepage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
