41ed2b8c7b0d5db63076d72cf46706daee561e8b
Mousa Aricat <21110078@htu.edu.jo>
2025-10-14 14:35:20 +0300

    Feat: adding the css file and starting with the nav bar and logo styling

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Quest, Task
+from .models import Quest, Task, Rank, Achievment, UserProfile
 
 # Register your models here.
 admin.site.register(Quest)
 admin.site.register(Task)
+admin.site.register(Rank)
+admin.site.register(Achievment)
+admin.site.register(UserProfile)
